373d6e79a00df9cf1bf16e7193a4590444f0b5f4
Eric Shelbogashev <96541869+EricShelbogashev@users.noreply.github.com>
2023-09-10 20:23:42 +0700

    added input

--- src/main/kotlin/bisection/Main.kt
+++ src/main/kotlin/bisection/Main.kt
@@ -1,6 +1,7 @@
 package bisection
 
 import java.time.Duration
+import java.util.Scanner
 
 fun `test twolinome 0`() {
     val a = -2.0
@@ -81,13 +82,25 @@ fun `test threelinome 5`() {
     ).forEach { println(it) }
 }
 
-
-
 fun main() {
+/*
     `test twolinome 0`()
     `test threelinome 1`()
     `test threelinome 2`()
     `test threelinome 3`()
     `test threelinome 4`()
     `test threelinome 5`()
+*/
+    val scanner = Scanner(System.`in`)
+    println("Enter: a b c epsilon")
+    val a = scanner.nextDouble()
+    val b = scanner.nextDouble()
+    val c = scanner.nextDouble()
+    val epsilon = scanner.nextDouble()
+    val polynomial = Threelynome(a, b, c)
+    polynomial.findRoots(
+        epsilon,
+        Duration.ofSeconds(3),
+        1.0
+    ).forEach { print(it) }
 }
